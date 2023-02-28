ead201bf0a1c35dec1373bb68f502471aa910bff
Viraj Balasaheb Patil <95068416+Virajpatil818@users.noreply.github.com>
2023-02-27 21:07:45 -0800

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.*;
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print(" Enter Numer :");
+        int n=sc.nextInt();
+        int a=n;
+        int p=0;
+        
+        while(n!=0){
+            p=p*10+n%10;
+            n=n/10;
+        }
+        
+        if(a==p)
+         System.out.println("Palindrome");  
+        else 
+         System.out.println("NOT Palindrome");
+    }
+}
